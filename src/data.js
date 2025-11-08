38ade322141eef7060bbec0bed9a11bd3d74243a
Lakshyaraj Singh Ranawat <lakshyaraj.singh.05@gmail.com>
2025-11-08 11:45:20 +0530

    Initial Setup

---
+++ src/data.js
@@ -0,0 +1,34 @@
+
+const cards=[
+    {
+     id:"jbhbjhbg",
+     title:"Applying React",
+     column:"todo"
+    },
+    {
+     id:"jbhbjhbg1",
+     title:"30 min sprint",
+     column:"todo"
+    },
+    {
+     id:"jbhbjhbg2",
+     title:"stretching",
+     column:"todo"
+    },
+    {
+     id:"jbhbjhbg3",
+     title:"GYM",
+     column:"inProgress"
+    },
+    {
+     id:"jbhbjhbg4",
+     title:"Fruits",
+     column:"inProgress"
+    },
+    {
+     id:"jbhbjhbg5",
+     title:"Coffee",
+     column:"Done"
+    }
+]
+    
